70578852fd763f03f6afd983e7aed33760664315
Anmol Raj Soni <anmolrajsoni15@gmail.com>
2022-06-19 14:41:30 +0530

    Final website

--- readme.md
+++ readme.md
@@ -1,1 +1,40 @@
-## LearnEd (E-learning Website)
+## EduSite (E-learning Website)💻
+<div id="About"></div>
+
+# About❓
+Education is the only thing that grows with Distribution.
+
+Education is the process of facilitating learning, or the acquisition of knowledge, skills, values,
+beliefs, and habits. Educational methods include teaching, training, storytelling, discussion and directed research!
+
+Our Educational Website would provide all the education related stuffs:
+Notes, Sample Papers, Online Video Lectures and courses to crack competitive
+exams like JEE-Main, JEE-Advanced, GATE, etc. Students can clear their doubts
+by sending their questions to our website. We have added Sample paper for Students
+who are willing to solve problems on different topics. We have also added
+Interview questions for students who are preparing for placements also providing login option.
+
+✔ We have made this website as responsive website so, students or users can
+easily access our website from any device.
+
+
+It is completely responsive website, to provide smooth experience...😎
+
+Show some ❤️ by starring⭐ this awesome Repository!
+
+# Tech-Stack🎓
+
+<img alt="HTML5" src="https://img.shields.io/badge/html5%20-%23E34F26.svg?&style=for-the-badge&logo=html5&logoColor=white"/> <img alt="CSS3" src="https://img.shields.io/badge/css3%20-%231572B6.svg?&style=for-the-badge&logo=css3&logoColor=white"/> <img alt="JavaScript" src="https://img.shields.io/badge/javascript%20-%23323330.svg?&style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"/>
+
+On Clicking LogIn or register, It will redirect to Google SignIn page .
+
+# Our GithubId:
+
+--> Anmol Raj Soni: https://github.com/anmolrajsoni15
+
+--> Shreya Mishra: https://github.com/shrey2003
+
+--> Shreyash Kumar: https://github.com/shreyashkr17
+
+# Project For:
+Project On EDUCATION:  WEB/MOBILE DEVELOPMENT HACKATHON Organised By NATIONAL INSTITUTE OF TECHNOLOGY (NIT), MIZORAM.
--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-// Changing the style of scroll bar
-// window.onscroll = function() {myFunction()};
-		
-// function myFunction() {
-// 	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-// 	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-// 	var scrolled = (winScroll / height) * 100;
-// 	document.getElementById("myBar").style.width = scrolled + "%"; 
-// }
-
 function scrollAppear() {
   var introText = document.querySelector('.side-text');
   var sideImage = document.querySelector('.sideImage');
   var introPosition = introText.getBoundingClientRect().top;
   var imagePosition = sideImage.getBoundingClientRect().top;
-  
+
   var screenPosition = window.innerHeight / 1.2;
 
-  if(introPosition < screenPosition) {
+  if (introPosition < screenPosition) {
     introText.classList.add('side-text-appear');
   }
-  if(imagePosition < screenPosition) {
+  if (imagePosition < screenPosition) {
     sideImage.classList.add('sideImage-appear');
   }
 }
@@ -29,15 +19,15 @@ window.addEventListener('scroll', scrollAppear);
 // For switching between navigation menus in mobile mode
 var i = 2;
 function switchTAB() {
-	var x = document.getElementById("list-switch");
-	if(i%2 == 0) {
-		document.getElementById("list-switch").style= "display: grid; height: 50vh; margin-left: 5%;";
-		document.getElementById("search-switch").style= "display: block; margin-left: 5%;";
-	}else {
-		document.getElementById("list-switch").style= "display: none;";
-		document.getElementById("search-switch").style= "display: none;";
-	}
-	i++;
+  var x = document.getElementById("list-switch");
+  if (i % 2 == 0) {
+    document.getElementById("list-switch").style = "display: grid; height: 50vh; margin-left: 5%;";
+    document.getElementById("search-switch").style = "display: block; margin-left: 5%;";
+  } else {
+    document.getElementById("list-switch").style = "display: none;";
+    document.getElementById("search-switch").style = "display: none;";
+  }
+  i++;
 }
 
 // For LOGIN
@@ -65,57 +55,24 @@ function login() {
   a.style.color = "#fff";
   b.style.color = "#000";
 }
-  
+
 // CheckBox Function
-function goFurther(){
+function goFurther() {
   if (document.getElementById("chkAgree").checked == true) {
-    document.getElementById('btnSubmit').style = 'background: linear-gradient(to right, #FA4B37, #DF2771);';
+    document.getElementById('btnSubmit').style = 'background: linear-gradient(to right, #6716ea, #dc3954)';
   }
-  else{
+  else {
     document.getElementById('btnSubmit').style = 'background: lightgray;';
   }
 }
 
 function google() {
-  	window.location.assign("https://accounts.google.com/signin/v2/identifier?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue&csig=AF-SEnbZHbi77CbAiuHE%3A1585466693&flowName=GlifWebSignIn&flowEntry=AddSession", "_blank");
+  window.location.assign("https://accounts.google.com/signin/v2/identifier?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue&csig=AF-SEnbZHbi77CbAiuHE%3A1585466693&flowName=GlifWebSignIn&flowEntry=AddSession", "_blank");
 }
 
-// QUIZ Page
-function quizt(frame) {
-  document.getElementById('f1').style='display: none;';
-  document.getElementById('f2').style='display: none;';
-  document.getElementById('f3').style='display: none;';
-  document.getElementById('f4').style='display: none;';
-  document.getElementById('f5').style='display: none;';
-  document.getElementById('f6').style='display: none;';
-  document.getElementById('f7').style='display: none;';
-  document.getElementById('f8').style='display: none;';
-  document.getElementById('f9').style='display: none;';
-  document.getElementById('f10').style='display: none;';
-  document.getElementById('f11').style='display: none;';
-  if(frame == 1) document.getElementById('f1').style = 'display: block';
-  else if(frame == 2) document.getElementById('f2').style = 'display: block';
-  else if(frame == 3) document.getElementById('f3').style = 'display: block';
-  else if(frame == 4) document.getElementById('f4').style = 'display: block';
-  else if(frame == 5) document.getElementById('f5').style = 'display: block';
-  else if(frame == 6) document.getElementById('f6').style = 'display: block';
-  else if(frame == 7) document.getElementById('f7').style = 'display: block';
-  else if(frame == 8) document.getElementById('f8').style = 'display: block';
-  else if(frame == 9) document.getElementById('f9').style = 'display: block';
-  else if(frame == 10) document.getElementById('f10').style = 'display: block';
-  else if(frame == 11) document.getElementById('f11').style = 'display: block'; 
-  else alert('error');
-}
 
-function startquiz() {
-  document.getElementById('title').style = 'display: none;'; 
-
-  document.getElementById('panel').style = 'display: inline-flex;'; 
-  document.getElementById('left').style = 'display: block;'; 
-  document.getElementById('right').style = 'display: block;'; 
-}
 function searchdisplay() {
-  document.getElementById('searchpanel').style.display="block";
+  document.getElementById('searchpanel').style.display = "block";
 }
 
 function display(n) {
@@ -137,28 +94,28 @@ function display(n) {
   s3.style = 'background: #DF2771; color: #FFF;';
   s4.style = 'background: #DF2771; color: #FFF;';
 
-  if(n==1) {
+  if (n == 1) {
     img1.style = 'display: block;';
     s1.style = 'background: #E5E8EF; color: #DF2771;';
   }
-  if(n==2) {
+  if (n == 2) {
     img2.style = 'display: block;';
     s2.style = 'background: #E5E8EF; color: #DF2771;';
   }
-  if(n==3) {
+  if (n == 3) {
     img3.style = 'display: block;';
     s3.style = 'background: #E5E8EF; color: #DF2771;';
   }
-  if(n==4) {
+  if (n == 4) {
     img4.style = 'display: block;';
     s4.style = 'background: #E5E8EF; color: #DF2771;';
-  } 
+  }
 }
 
 
 function sideMenu(side) {
   var menu = document.getElementById('side-menu');
-  if(side==0) {
+  if (side == 0) {
     menu.style = 'transform: translateX(0vh); position:fixed;';
   }
   else {
